e0b9a4aa2019210d22391f92bd69a19ed5f171da
Wilson Teh <ywteh13@gmail.com>
2021-06-07 15:47:05 +0800

    Make backers card
    1. Apply HTML & CSS to backers card
    2. Make backers card responsive in any screen size (>=320px)
    3. Apply necessary color to slider based on percentage of money backed

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const body = document.body;
+
 const nav = body.querySelector('#nav');
 const toggle = body.querySelector('#toggle');
+const slidebar = body.querySelector('#slidebar');
 
+const moderateCyan = 'hsl(176, 50%, 47%)';
+
+console.log(slidebar)
 // console.dir(toggle)
 
 
@@ -14,6 +19,22 @@ function toggleNav() {
     nav.classList.toggle('toggle-nav');
 }
 
+function slidebarColor(slidebar) {
+    let amountBacked = body.querySelector('.amount-backed .main span').textContent;
+    let totalAmount = body.querySelector('.amount-backed .sub span').textContent;
+    
+    amountBacked = amountBacked.replace(',', '');
+    totalAmount = totalAmount.replace(',', '');
+    
+    backedPercent = (amountBacked/ totalAmount * 100)
+
+    slidebar.style.background = 
+    `linear-gradient(to right, ${moderateCyan} 0%, ${moderateCyan} ${backedPercent}%, hsl(0, 0%, 92%) ${backedPercent}%)`
+
+}
+
+slidebarColor(slidebar);
 toggle.addEventListener('change', toggleNav)
+
 
 
